2c1355cb61e949b1bcd55f80d74d63a6f86f8a18
FacuOrellana <facundo.ore@hotmail.com>
2022-03-22 16:31:56 -0300

    a

--- src/api/GlobalApiConfs.js
+++ src/api/GlobalApiConfs.js
@@ -4,6 +4,6 @@
 // export const rootApiRoute = "http://localhost:54156";
 
 export const sessionApiRoute = "https://tp1idsg15webservice20220301161756.azurewebsites.net/sessions";
-export const rootApiRoute = "https://tp1idsg15webservice20220301161756.azurewebsites.net/";
+export const rootApiRoute = "https://tp1idsg15webservice20220301161756.azurewebsites.net";
 // export const sessionApiRoute = "http://localhost:59128/sessions";
 // export const rootApiRoute = "http://localhost:59128";
