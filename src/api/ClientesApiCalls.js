784839a9d0b85ef98a55ab978dc8826d92d6c7d1
FacuOrellana <facundo.ore@hotmail.com>
2022-03-22 21:32:50 -0300

    aa

--- src/api/ClientesApiCalls.js
+++ src/api/ClientesApiCalls.js
@@ -10,21 +10,11 @@ export const getAllClientesApiCall = async () => {
         throw error;
     }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const getClienteApiCall = async (id) => {
-    try {
-        const response = await axios.get(rootApiRoute + "/clientes/"+id);
-=======
+       
 export const getClienteApiCall = async (cuit) => {
     try {
         const response = await axios.get(rootApiRoute + "/clientes/?cuit="+cuit);
->>>>>>> 0d7372a03e92bd5a30bcc0ef5abeb8c307b18557
-=======
-export const getClienteApiCall = async (cuit) => {
-    try {
-        const response = await axios.get(rootApiRoute + "/clientes/?cuit="+cuit);
->>>>>>> origin
+
         return response.data;
     } catch (error) {
         throw error;
@@ -33,57 +23,39 @@ export const getClienteApiCall = async (cuit) => {
 
 export const createNewClienteApiCall = async (cliente) => {
     try {
-<<<<<<< HEAD
-<<<<<<< HEAD
         const response = await axios.post(rootApiRoute + "/clientes", cliente);
-=======
-        const response = await axios.post(rootApiRoute + "/clientes/", cliente);
->>>>>>> 0d7372a03e92bd5a30bcc0ef5abeb8c307b18557
-=======
-        const response = await axios.post(rootApiRoute + "/clientes/", cliente);
->>>>>>> origin
+
         return response;
     } catch (error) {
         throw error;
     }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export const updateClienteApiCall = async (id, cliente) => {
     try {
         const response = await axios.put(rootApiRoute + "/clientes/"+id, cliente);
-=======
+
 export const saveClienteApiCall = async (cliente) => {
     try {
         const response = await axios.put(rootApiRoute + "/clientes/", cliente);
->>>>>>> 0d7372a03e92bd5a30bcc0ef5abeb8c307b18557
-=======
+
+
 export const updateClienteApiCall = async (id, cliente) => {
     try {
         const response = await axios.put(rootApiRoute + "/clientes/"+id, cliente);
->>>>>>> origin
+
         return response;
     } catch (error) {
         throw error;
     }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const deleteClienteApiCall = async (id) => {
-    try {
-        const response = await axios.delete(rootApiRoute + "/clientes/"+id);
-=======
+
 export const deleteClienteApiCall = async (cuit) => {
     try {
         const response = await axios.delete(rootApiRoute + "/clientes/"+ cuit);
->>>>>>> 0d7372a03e92bd5a30bcc0ef5abeb8c307b18557
-=======
-export const deleteClienteApiCall = async (cuit) => {
-    try {
-        const response = await axios.delete(rootApiRoute + "/clientes/"+ cuit);
->>>>>>> origin
+
         return response;
     } catch (error) {
         throw error;
